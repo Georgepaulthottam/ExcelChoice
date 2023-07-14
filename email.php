aaf1982f521752f5574087ce5022a5c0d662e8c1
Georgepaulthottam <78588483+Georgepaulthottam@users.noreply.github.com>
2023-07-14 11:43:58 +0530

    y

---
+++ email.php
@@ -0,0 +1,16 @@
+<?php
+
+ 
+if($_POST["submit"]) {
+
+ 
+mail("georgepaulthottam@gmail.com", "Here is the subject line",
+
+ 
+$_POST["insert your message here"]. "From: excelchoie@gmail.com");
+
+ 
+}
+
+ 
+?>
